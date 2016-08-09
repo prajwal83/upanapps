febaca67248991fbb371dd19b558502ff8cda757
Prajwala Prabhakar <srinivasa_prajwal@yahoo.co.in>
2016-08-09 21:42:36 +0800

    Update process status enum description list

--- libc/stdlib/stdlib.c
+++ libc/stdlib/stdlib.c
@@ -840,7 +840,7 @@ void format_dir_attr(unsigned short attr, char* fmAttr)
 }
 
 static const char PROC_STATUS_DESC[TOTAL_PROC_STATUS + 1][30] = {
-	"New", "Run", "Sleep", "Wait(int)", "Wait(child)", "Wait(resource)", "unknown" } ;
+	"New", "Run", "Sleep", "Wait(int)", "Wait(child)", "Wait(resource)", "Wait(service)", "Wait(event)", "unknown" } ;
 
 const char* getProcStatusDesc(int iStatus)
 {
